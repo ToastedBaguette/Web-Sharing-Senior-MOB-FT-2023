252df45ff88c796eda2c4fc7852c26b3b6e12387
ToastedBaguette <ronyhartonoirawan@gmail.com>
2023-07-20 23:06:49 +0700

    Update Frontend

---
+++ resources/views/home.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Sharing Senior MOB FT 2023</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/logo.png') }}">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/broken-console" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/04b30" rel="stylesheet">
+
+    <!-- Style -->
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/custom.css') }}">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-4 col-6 p-2 p-md-5">
+                <div class="card">
+                    <img src="{{ asset('img/background.jpg') }}" class="card-img-top" alt="Petuah">
+                    <div class="card-body">
+                        <h5 class="card-title">Nama Petuah</h5>
+                        <p class="badge rounded-pill text-bg-success">Success</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-6 p-2 p-md-5">
+                <div class="card">
+                    <img src="{{ asset('img/background.jpg') }}" class="card-img-top" alt="Petuah">
+                    <div class="card-body">
+                        <h5 class="card-title">Nama Petuah</h5>
+                        <p class="badge rounded-pill text-bg-success">Success</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-6 p-2 p-md-5">
+                <div class="card">
+                    <img src="{{ asset('img/background.jpg') }}" class="card-img-top" alt="Petuah">
+                    <div class="card-body">
+                        <h5 class="card-title">Nama Petuah</h5>
+                        <p class="badge rounded-pill text-bg-success">Success</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-6 p-2 p-md-5">
+                <div class="card">
+                    <img src="{{ asset('img/background.jpg') }}" class="card-img-top" alt="Petuah">
+                    <div class="card-body">
+                        <h5 class="card-title">Nama Petuah</h5>
+                        <p class="badge rounded-pill text-bg-success">Success</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
