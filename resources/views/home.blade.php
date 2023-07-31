bd28cdfecd7b0c9cdb800156b168b6e1526378dd
alexanderkenrick <alexanderkenrick3@gmail.com>
2023-07-31 16:48:15 +0700

    Home peserta

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,10 +18,27 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <style>
+        .card-kelompok .badge{
+            width:100px;
+        }
+    </style>
 </head>
 
 <body style="background-color: #120238;">
     <div class="container">
+        <div class="row mt-3">
+            <div class="col-md-6 p3 p-md-4">
+                <div class="card card-kelompok">
+                    <div class="card-body">
+                        <p class="card-text">Nama : Kelompok 1</p>
+                        <p class="m-0">Status : <span class="badge bg-success">Accepted</span></p>
+                        {{-- <p>Status : <span class="badge bg-danger">Not Accepted</span></p>
+                        <p>Status : <span class="badge bg-warning">Waiting</span></p> --}}
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="row">
             @foreach ($seniors as $senior)
                 {{-- Card Petuah --}}
@@ -30,7 +47,7 @@
                         <div class="row g-0">
                             <div class="d-flex col-4">
                                 <img src="{{ asset('img/petuah_placeholder.jpg') }}" style="object-fit: cover"
-                                    class="img-fluid rounded-start"">
+                                    class="img-fluid rounded-start">
                             </div>
                             <div class="col-8">
                                 <div class="card-body h-100 d-flex align-items-center">
