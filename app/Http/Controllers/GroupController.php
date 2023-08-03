18f8c2c2834c641fa29413d41a67b34d78d50cb1
jryand25 <jonathan.ryan.darmawan@gmail.com>
2023-08-03 23:08:24 +0700

    Update GroupController.php

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -15,37 +15,40 @@ class GroupController extends Controller
 {
     public function index()
     {
-        $seniors = User::where('role','Senior')->get();
+        $seniors = User::where('role', 'Senior')->get();
 
         $user = Auth::user();
-        $group = Group::where('user_id',$user->id)->first();
-        $requests = DB::table('requests')->where('group_id',$group->id)->where('status','ACCEPTED')->get();
-        if(count($requests) != 0){
+        $group = Group::where('user_id', $user->id)->first();
+        $requests = DB::table('requests')->where('group_id', $group->id)->where('status', 'ACCEPTED')->get();
+        if (count($requests) != 0) {
             $senior_id = $requests[0]->senior_id;
             $senior = Senior::find($senior_id);
             $senior_name = $senior->user->name;
             $info = $senior_name . " (" . $senior->location . ")";
             $accepted_id = $senior_id;
-        }
-        $requests = DB::table('requests')->where('group_id',$group->id)->where('status','WAITING')->get();
+        } else {
 
-        if(count($requests) != 0){
-            $senior_id = $requests[0]->senior_id;
-            $senior = Senior::find($senior_id);
-            $senior_name = $senior->user->name;
-            $info = $senior_name;
-            $accepted_id = "";
-        }else{
-            $accepted_id = "";
-            $info = "-";
+            $requests = DB::table('requests')->where('group_id', $group->id)->where('status', 'WAITING')->get();
+
+            if (count($requests) != 0) {
+                $senior_id = $requests[0]->senior_id;
+                $senior = Senior::find($senior_id);
+                $senior_name = $senior->user->name;
+                $info = $senior_name;
+                $accepted_id = "";
+            } else {
+                $accepted_id = "";
+                $info = "-";
+            }
         }
         return view('home', compact('seniors', 'group', 'info', 'accepted_id'));
     }
 
-    function request(Request $request) {
+    function request(Request $request)
+    {
         $senior_id = (int)$request->senior_id;
         $user = Auth::user();
-        $group = Group::where('user_id',$user->id)->first();
+        $group = Group::where('user_id', $user->id)->first();
         $group_id = $group->id;
         $group->increment('is_waiting');
         DB::table('requests')->insert([
