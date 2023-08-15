6855903720b86da491844aab858fa8785adb10f0
jryand25 <jonathan.ryan.darmawan@gmail.com>
2023-08-15 18:03:48 +0700

    revisi

--- resources/views/senior.blade.php
+++ resources/views/senior.blade.php
@@ -172,7 +172,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="staticBackdropLabel">History</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <section class="accepted-section">
@@ -202,9 +202,7 @@
 
                 </div>
                 <div class="modal-footer">
-
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Understood</button>
                 </div>
             </div>
         </div>
